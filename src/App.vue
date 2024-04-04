25d2cf4dc6d6af340a2e092f3c3b791fa81e6aea
Mathieu Domichard <mathieu.domichard@gmail.com>
2024-04-04 17:32:19 +0200

    ajout du background

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="container">
-    <div v-for="(card, index) in cards" :key="index" @click="handleCardClick(index)" class="card">
-      <div v-if="!card.flipped" class="card-front"> <!-- Affiche le contenu de la carte à l'avant -->
+    <div
+      v-for="(card, index) in cards"
+      :key="index"
+      @click="handleCardClick(index)"
+      class="card"
+    >
+      <div v-if="!card.flipped" class="card-front">
+        <!-- Affiche le contenu de la carte à l'avant -->
         ?
       </div>
-      <div v-else class="card-back"> <!-- Affiche le contenu de la carte à l'arrière -->
+      <div v-else class="card-back">
+        <!-- Affiche le contenu de la carte à l'arrière -->
         {{ card.content }}
       </div>
     </div>
@@ -12,60 +19,59 @@
 </template>
 
 <script>
-import { ref, onBeforeMount } from 'vue';
+import { ref, onBeforeMount } from "vue";
 
 export default {
   setup() {
     const cards = ref([
-      { content: 'X', flipped: false },
-      { content: 'X', flipped: false },
-      { content: 'X', flipped: false },
-      { content: 'X', flipped: false },
-      { content: 'X', flipped: false },
-      { content: 'X', flipped: false },
-      { content: 'X', flipped: false },
-      { content: 'X', flipped: false },
-      { content: 'X', flipped: false },
+      { content: "X", flipped: false },
+      { content: "X", flipped: false },
+      { content: "X", flipped: false },
+      { content: "X", flipped: false },
+      { content: "X", flipped: false },
+      { content: "X", flipped: false },
+      { content: "X", flipped: false },
+      { content: "X", flipped: false },
+      { content: "X", flipped: false },
     ]);
     let clickCount = ref(0);
     const maxClicks = 3;
 
     const handleCardClick = (index) => {
-  const currentCard = cards.value[index];
-  if (!currentCard.flipped) {
-    currentCard.flipped = true; // Inverser la carte
-    if (currentCard.content === 'chat') {
-      alert('Vous avez trouvé le chat!');
-      resetGame();
-    } else {
-      clickCount.value++;
-      if (clickCount.value === maxClicks) {
-        const newIndex = Math.floor(Math.random() * cards.value.length);
-        cards.value[newIndex].content = 'chat';
-        cards.value[newIndex].flipped = false; // Assurez-vous que la nouvelle carte contenant le chat n'est pas retournée
-        resetGame();
+      const currentCard = cards.value[index];
+      if (!currentCard.flipped) {
+        currentCard.flipped = true; // Inverser la carte
+        if (currentCard.content === "chat") {
+          alert("Vous avez trouvé le chat!");
+          resetGame();
+        } else {
+          clickCount.value++;
+          if (clickCount.value === maxClicks) {
+            const newIndex = Math.floor(Math.random() * cards.value.length);
+            cards.value[newIndex].content = "chat";
+            cards.value[newIndex].flipped = false; // Assurez-vous que la nouvelle carte contenant le chat n'est pas retournée
+            resetGame();
+          }
+        }
       }
-    }
-  }
-};
+    };
 
+    const initializeGame = () => {
+      const chatIndex = Math.floor(Math.random() * cards.value.length);
+      cards.value[chatIndex].content = "chat";
+    };
 
-const initializeGame = () => {
-  const chatIndex = Math.floor(Math.random() * cards.value.length);
-  cards.value[chatIndex].content = 'chat';
-};
+    // Dans la méthode resetGame
+    const resetGame = () => {
+      clickCount.value = 0;
+      cards.value.forEach((card) => {
+        card.flipped = false;
+        card.content = "X"; // Réinitialiser le contenu de toutes les cartes à 'X'
+      });
+      initializeGame(); // Replacer le chat sur une carte aléatoire
+    };
 
-// Dans la méthode resetGame
-const resetGame = () => {
-  clickCount.value = 0;
-  cards.value.forEach(card => {
-    card.flipped = false;
-    card.content = 'X'; // Réinitialiser le contenu de toutes les cartes à 'X'
-  });
-  initializeGame(); // Replacer le chat sur une carte aléatoire
-};
-
-onBeforeMount(() => {
+    onBeforeMount(() => {
       initializeGame();
     });
 
@@ -78,17 +84,23 @@ onBeforeMount(() => {
 </script>
 
 <style>
-
-html, body {
-  height: 100%; 
-  margin: 0; 
-  padding: 0; 
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 
 body {
-  display: flex; 
-  justify-content: center; 
-  align-items: center; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgb(28, 229, 233);
+  background: linear-gradient(
+    90deg,
+    rgba(28, 229, 233, 1) 0%,
+    rgba(161, 195, 186, 1) 56%
+  );
 }
 /* Styles pour la grille et les cartes */
 .container {
@@ -100,7 +112,7 @@ body {
 }
 
 .card-front {
-  background-color: #FFD700; /* Or */
+  background-color: #ffd700; /* Or */
   min-height: 250px;
   width: 250px;
   display: flex;
@@ -110,7 +122,7 @@ body {
 }
 
 .card-back {
-  background-color: #6495ED; /* Bleu cornflower */
+  background-color: #6495ed; /* Bleu cornflower */
   min-height: 250px;
   width: 250px;
   display: flex;
@@ -118,5 +130,4 @@ body {
   align-items: center;
   font-size: 24px;
 }
-
 </style>
